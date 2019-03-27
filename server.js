54dfaefc2f2d16ff533febf34b6fd5fdce18f981
Guybrush Threepwood <benoit.development@gmail.com>
2019-03-27 10:42:46 +0100

    First App init

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import db from './app/db/db';
+
+// Set up the express app
+const app = express();
+
+// get all todos
+app.get('/api/v1/todos', (req, res) => {
+  console.log(`Request Todos`)
+  res.status(200).send({
+    success: 'true',
+    message: 'todos retrieved successfully',
+    todos: db
+  })
+});
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`)
+});
