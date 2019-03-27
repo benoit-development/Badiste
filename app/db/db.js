fa502b9d098d9668352efb65a8501d1171b2f5ba
Guybrush Threepwood <benoit.development@gmail.com>
2019-03-27 10:46:02 +0100

    new data

--- app/db/db.js
+++ app/db/db.js
@@ -1,9 +1,14 @@
 const todos =  [
-    {
-      id: 1,
-      title: "lunch",
-      description: "Go for lunc by 2pm"
-    }
+  {
+    id: 1,
+    title: "lunch",
+    description: "Go for lunc by 2pm"
+  },
+  {
+    id: 2,
+    title: "repair car",
+    description: "the light is not good"
+  }
 ];
 
 export default todos;
