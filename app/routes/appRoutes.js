4a3c7aefa067f7f675a1892b7147135503db37ba
Benoît BOUSQUET <benoit.development@gmail.com>
2019-04-05 10:50:58 +0200

    Compression Module + little route optimization

--- app/routes/appRoutes.js
+++ app/routes/appRoutes.js
@@ -4,9 +4,9 @@ import { find_players, find_players_by_license, save_player } from '../controlle
 var router = require('express').Router();
 
 // Players routes
-router.get('/api/v1/players', find_players);
-router.post('/api/v1/players', save_player);
-router.get('/api/v1/players?search=:search', find_players);
-router.get('/api/v1/players/:license', find_players_by_license);
+router.get('/players', find_players);
+router.post('/players', save_player);
+router.get('/players?search=:search', find_players);
+router.get('/players/:license', find_players_by_license);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,12 +6,14 @@ const routes = require('./app/routes/appRoutes');
 const bodyParser = require('body-parser');
 const PORT = 5000;
 var cors = require('cors');
+var compression = require('compression');
 
 // configure app
 app.use(cors());
+app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/', routes);
+app.use('/api/v1', routes);
 
 // server listen
 app.listen(PORT, () => {
